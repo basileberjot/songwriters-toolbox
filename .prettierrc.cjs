110c52b7e23a3e54dd4bbe4617d64c8e449bd5f8
Basile <basileberjot@gmail.com>
2023-03-09 11:36:20 +0900

    Update Songwriters toolbox

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,6 +2,12 @@ module.exports = {
   plugins: [require.resolve("prettier-plugin-astro")],
   overrides: [
     {
+      files: ["*.astro", "*.svelte"],
+      options: {
+        parser: "svelte",
+      },
+    },
+    {
       files: "*.astro",
       options: {
         parser: "astro",
---
+++ src/components/Greeting.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { onMount, onDestroy } from 'svelte';
+  
+    export let messages = [];
+  
+    let greeting = '';
+    
+    const setRandomGreeting = () => {
+        greeting = messages[Math.floor(Math.random() * messages.length)];
+    };
+
+    const handleButtonClick = () => {
+        setRandomGreeting();
+    };
+    
+    onMount(() => {
+        setRandomGreeting();
+    });
+    
+    onDestroy(() => {
+        greeting = '';
+    });
+    </script>
+  
+  <div>
+    <h3>{greeting}! Thank you for visiting!</h3>
+    <button on:click={handleButtonClick}>
+      New Greeting
+    </button>
+  </div>
